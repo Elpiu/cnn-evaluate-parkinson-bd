9b14bb7693474a750c5d853a175863b778077c31
Elpiu <49283847+Elpiu@users.noreply.github.com>
2022-04-27 23:19:49 +0200

    idk

--- FristSimpleModel/fristRunModel.py
+++ FristSimpleModel/fristRunModel.py
@@ -18,12 +18,23 @@ model.summary()
 
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, verbose=2)
+model.fit(x=scaled_train_samples, y=train_labels,
+            validation_split=0.1, batch_size=10, epochs=30,
+            shuffle=True, verbose=2)
+
+#validation_split
+v = "/"*10+"Testing"+"/"*10
+print(v*10)
+from testData import *
+predictions = model.predict(
+      x=scaled_test_samples
+    , batch_size=10
+    , verbose=0
+)  
 
 
-
-
-
+for i in predictions:
+    print(i)
 
 
 
